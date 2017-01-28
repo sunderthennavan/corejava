2f8d7e696a6e2d1a9346774f3ac6236d85d4dd59
ATHENA <sunder.nit@gmail.com>
2017-01-28 06:10:55 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,22 @@
+/**
+ * @(#)hello.java
+ *
+ *
+ * @author 
+ * @version 1.00 2017/1/26
+ */
+
+import java.io.*; 
+import java.util.*;
+
+public class hello {
+
+    public static void main (String[] args) {
+    
+    System.out.println("Welcome to Java");
+    
+    
+    }
+    
+    
+}
