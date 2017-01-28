ac9c15529de7db2ed6bc7b954153802325c475d8
ATHENA <sunder.nit@gmail.com>
2017-01-28 07:12:21 +0530

    Revert "Revert "Add files via upload""
    This reverts commit 6c8e508429fb4223f569d49c70b2c62eca4958f5.

---
+++ stringcharacInsert.java
@@ -0,0 +1,28 @@
+/*
+@Author:Sunder
+@Code: String Character Insertion
+@Date:28-01-2017
+*/
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class stringcharacInsert{
+
+
+  public static void main(String[] args){
+
+
+   String Name = "Athena";
+   String s = Name.substring(0,5);
+
+   System.out.println(s+"s"); //calling substring as well as adding character
+
+  }
+
+
+
+}
